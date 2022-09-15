4192161d307ee67b33baf1f11147a5f1a30d928e
ilyasbabu <ilyasbabuvp@gmail.com>
2022-09-15 18:04:48 +0530

    star component made reusable

--- frontend/src/components/ProductCard.js
+++ frontend/src/components/ProductCard.js
@@ -9,97 +9,12 @@ function ProductCard({ product }) {
   return (
     <div className="w-96 flex justify-center items-center relative ">
 
-
-    <StarRating starCount={product.rating} reviewCount={product.numReviews} />
-
-
-      {/* <span
+      <span
         className="absolute z-10 inline-flex items-center px-3 py-1 text-xs font-semibold text-slate-600  bg-white rounded-md left-10 top-7"
       >
-        <ul class="flex items-center">
-          <li>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="w-4 h-4 text-yellow-300 fill-current hover:text-yellow-400"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
-              />
-            </svg>
-          </li>
-          <li>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="w-4 h-4 text-yellow-300 fill-current hover:text-yellow-400"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
-              />
-            </svg>
-          </li>
-          <li>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="w-4 h-4 text-yellow-300 fill-current hover:text-yellow-400"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
-              />
-            </svg>
-          </li>
-          <li>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="w-4 h-4 text-yellow-300 fill-current hover:text-yellow-400"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
-              />
-            </svg>
-          </li>
-          <li>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="w-4 h-4 text-yellow-300"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
-              />
-            </svg>
-          </li>
-          <li class="ml-2 text-xs text-gray-600">{product.numReviews}</li>
-        </ul>
-      </span> */}
+        <StarRating starCount={product.rating} text={`${product.rating} FROM ${product.numReviews} REVIEWS`} />
+      </span>
+
 
 
 
--- frontend/src/components/StarRating.js
+++ frontend/src/components/StarRating.js
@@ -1,29 +1,25 @@
 import React from 'react'
 
-function StarRating({ starCount, reviewCount }) {
+function StarRating({ starCount, text }) {
     return (
-        <span
-            className="absolute z-10 inline-flex items-center px-3 py-1 text-xs font-semibold text-slate-600  bg-white rounded-md left-10 top-7"
-        >
-            <ul className="flex items-center">
-                <li>
-                    <i className={`w-4 h-4 text-yellow-300 hover:text-yellow-400 ${starCount >= 1 ? "fas fa-star" : starCount >= 0.5 ? "fas fa-star-half-alt" : "far fa-star"}`}></i>
-                </li>
-                <li>
-                    <i className={`w-4 h-4 text-yellow-300 hover:text-yellow-400 ${starCount >= 2 ? "fas fa-star" : starCount >= 1.5 ? "fas fa-star-half-alt" : "far fa-star"}`}></i>
-                </li>
-                <li>
-                    <i className={`w-4 h-4 text-yellow-300 hover:text-yellow-400 ${starCount >= 4 ? "fas fa-star" : starCount >= 2.5 ? "fas fa-star-half-alt" : "far fa-star"}`}></i>
-                </li>
-                <li>
-                    <i className={`w-4 h-4 text-yellow-300 hover:text-yellow-400 ${starCount >= 4 ? "fas fa-star" : starCount >= 3.5 ? "fas fa-star-half-alt" : "far fa-star"}`}></i>
-                </li>
-                <li>
-                    <i className={`w-4 h-4 text-yellow-300 hover:text-yellow-400 ${starCount >= 5 ? "fas fa-star" : starCount >= 4.5 ? "fas fa-star-half-alt" : "far fa-star"}`}></i>
-                </li>
-                <li className="ml-2 text-xs text-gray-600">{reviewCount}</li>
-            </ul>
-        </span>
+        <ul className="flex items-center">
+            <li>
+                <i className={`w-full h-full text-yellow-300 hover:text-yellow-400 ${starCount >= 1 ? "fas fa-star" : starCount >= 0.5 ? "fas fa-star-half-alt" : "far fa-star"}`}></i>
+            </li>
+            <li>
+                <i className={`w-full h-full text-yellow-300 hover:text-yellow-400 ${starCount >= 2 ? "fas fa-star" : starCount >= 1.5 ? "fas fa-star-half-alt" : "far fa-star"}`}></i>
+            </li>
+            <li>
+                <i className={`w-full h-full text-yellow-300 hover:text-yellow-400 ${starCount >= 4 ? "fas fa-star" : starCount >= 2.5 ? "fas fa-star-half-alt" : "far fa-star"}`}></i>
+            </li>
+            <li>
+                <i className={`w-full h-full text-yellow-300 hover:text-yellow-400 ${starCount >= 4 ? "fas fa-star" : starCount >= 3.5 ? "fas fa-star-half-alt" : "far fa-star"}`}></i>
+            </li>
+            <li>
+                <i className={`w-full h-full text-yellow-300 hover:text-yellow-400 ${starCount >= 5 ? "fas fa-star" : starCount >= 4.5 ? "fas fa-star-half-alt" : "far fa-star"}`}></i>
+            </li>
+            <li className="ml-2 text-xs text-gray-600">{text && text}</li>
+        </ul>
     )
 }
 

70d852ebab4c9d79744c3476e7ad14d8c7948673
ilyasbabu <ilyasbabuvp@gmail.com>
2022-10-07 17:58:18 +0530

    database designed

--- apis/models.py
+++ apis/models.py
@@ -1,3 +1,72 @@
+from email.policy import default
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class BaseModel(models.Model):
+    is_active = models.BooleanField(default = True)
+    created_time = models.DateTimeField(auto_now_add = True)
+    modified_time = models.DateTimeField(auto_now = True)
+    added_by = models.ForeignKey(User, on_delete = models.SET_NULL)
+
+
+class Product(BaseModel):
+    CATEGORY_CHOICES = (
+        ('Mobile Phone','Mobile Phone'),
+        ('Tablet','Tablet'),
+        ('Watch','Watch'),
+        ('Headphone','Headphone'),
+        ('Speaker','Speaker')
+    )
+    name = models.CharField(max_length = 100)
+    description = models.TextField()
+    brand = models.CharField(max_length = 50)
+    category = models.CharField(max_length = 15,choices = CATEGORY_CHOICES)
+    price = models.DecimalField(max_digits = 7, decimal_places = 2)
+    stock = models.IntegerField(default = 0)
+    rating = models.DecimalField(max_digits = 2, decimal_places = 1)
+    rating_count = models.IntegerField(default = 0)
+
+    # @property
+    # def avg(self):
+    #     return 'self'
+
+    # @classmethod
+    # def update_rating(cls):
+    #     print (cls)
+
+class ProductImage(BaseModel):
+    path = models.ImageField(upload_to = 'images/product')
+    is_main = models.BooleanField(default = False)
+    product = models.ForeignKey(Product, on_delete = models.CASCADE)
+
+
+class Review(BaseModel):
+    product = models.ForeignKey(Product, on_delete = models.CASCADE, related_name = 'product_rating')
+    rating = models.IntegerField()
+    heading = models.CharField(max_length = 100, null = True, blank = True)
+    description = models.TextField(null = True, blank = True)
+    helpful_count = models.IntegerField(null = True, blank = True)
+
+
+class Order(BaseModel):
+    payment_type = models.CharField(max_length = 100)
+    tax = models.DecimalField(max_digits = 7, decimal_places = 2)
+    shipping_charge = models.DecimalField(max_digits = 7, decimal_places = 2)
+    total = models.DecimalField(max_digits = 7, decimal_places = 2)
+    is_paid = models.BooleanField(default = False)
+    paid_at = models.DateTimeField(blank = True, null = True)
+    order_confirmed = models.BooleanField(default = False)
+    order_confirmed_at = models.DateTimeField(blank = True, null = True)
+    delivered = models.BooleanField(default = False)
+    delivered_at = models.DateTimeField(blank = True, null = True)
+
+ 
+class CartItem(BaseModel):
+    product = models.ForeignKey(Product, on_delete = models.CASCADE)
+    order = models.ForeignKey(Order, on_delete = models.CASCADE)
+    price = models.DecimalField(max_digits = 7, decimal_places = 2)
+    quantity = models.IntegerField(default = 0)
+
+    
--- backend/settings.py
+++ backend/settings.py
@@ -11,11 +11,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-import environ
+from dotenv import load_dotenv
+import os
 
-env = environ.Env()
-# reading .env file
-environ.Env.read_env()
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

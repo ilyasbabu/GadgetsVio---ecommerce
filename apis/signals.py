121184489eb3f8b1102ed5319fbf286eadd785ae
ilyasbabu <ilyasbabuvp@gmail.com>
2022-10-25 13:42:45 +0530

    signal for average rating

--- apis/signals.py
+++ apis/signals.py
@@ -1,9 +1,15 @@
 from django.db.models.signals import pre_save,post_save
 from django.dispatch import receiver
-from .models import Product, Review
+from .models import Review
+from django.db.models import Avg
 
 
 @receiver(post_save, sender=Review)
 def update_avg_raitng(sender, instance, created, **kwargs):
-    print(sender, instance, created)
-    
+    product_instance = instance.product
+    reviews = Review.objects.filter(product = product_instance, is_active = True)
+    avg_rating = reviews.aggregate(Avg('rating'))['rating__avg']
+    rating_count = reviews.count()
+    product_instance.avg_rating = avg_rating
+    product_instance.rating_count = rating_count
+    product_instance.save()

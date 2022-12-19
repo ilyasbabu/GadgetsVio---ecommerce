3b8836ab4fa16bbb59a4eb43844a8a370f2696a8
ilyasbabu <ilyasbabuvp@gmail.com>
2022-12-19 17:55:00 +0530

    Cart page summary section

--- frontend/src/components/CartSummarySection.js
+++ frontend/src/components/CartSummarySection.js
@@ -1,28 +1,28 @@
 import React from 'react'
 
-function CartSummarySection() {
+function CartSummarySection({ total_price, total_qty, no_of_items }) {
     return (
         <div className='lg:w-96 w-full bg-gray-100 dark:bg-gray-900 h-full right-0 lg:fixed'>
             <div className="flex flex-col lg:w-96 w-full lg:h-screen h-auto lg:px-8 md:px-7 px-4 lg:py-20 md:py-10 py-6 justify-between overflow-y-auto ">
                 <div>
                     <p className="lg:text-4xl text-3xl font-black leading-9 text-gray-800 dark:text-white">Summary</p>
                     <div className="flex items-center justify-between pt-16">
-                        <p className="text-base leading-none text-gray-800 dark:text-white">Subtotal</p>
-                        <p className="text-base leading-none text-gray-800 dark:text-white">000</p>
+                        <p className="text-base leading-none text-gray-800 dark:text-white">Number of items</p>
+                        <p className="text-base leading-none text-gray-800 dark:text-white">{no_of_items}</p>
                     </div>
                     <div className="flex items-center justify-between pt-5">
-                        <p className="text-base leading-none text-gray-800 dark:text-white">Shipping</p>
-                        <p className="text-base leading-none text-gray-800 dark:text-white">000</p>
+                        <p className="text-base leading-none text-gray-800 dark:text-white">Total Quantity</p>
+                        <p className="text-base leading-none text-gray-800 dark:text-white">{total_qty}</p>
                     </div>
                     <div className="flex items-center justify-between pt-5">
-                        <p className="text-base leading-none text-gray-800 dark:text-white">Tax</p>
-                        <p className="text-base leading-none text-gray-800 dark:text-white">000</p>
+                        <p className="text-base leading-none text-gray-800 dark:text-white">Total price</p>
+                        <p className="text-base leading-none text-gray-800 dark:text-white">{total_price}</p>
                     </div>
                 </div>
                 <div>
                     <div className="flex items-center pb-6 justify-between lg:pt-5 pt-20">
                         <p className="text-2xl leading-normal text-gray-800 dark:text-white">Total</p>
-                        <p className="text-2xl font-bold leading-normal text-right text-gray-800 dark:text-white">$ 240000</p>
+                        <p className="text-2xl font-bold leading-normal text-right text-gray-800 dark:text-white">$ {total_price}</p>
                     </div>
                     <button onClick={()=>{}} className="text-base leading-none w-full py-5 bg-gray-800 border-gray-800 border focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800 text-white dark:hover:bg-gray-700">Checkout</button>
                 </div>
--- frontend/src/pages/CartPage.js
+++ frontend/src/pages/CartPage.js
@@ -9,33 +9,37 @@ import { getCartItemsAsync } from '../features/cartSlice';
 function CartPage() {
     console.log("asdasdsadsadsa");
     const { cartItems, total_price, total_qty, error_, loading } = useSelector(state => state.cart_items)
-    const { message, type} = useSelector(state => state.commons)
+    const { message, type } = useSelector(state => state.commons)
     const dispatch = useDispatch()
     useEffect(() => {
         window.scrollTo(0, 0);
         dispatch(getCartItemsAsync())
     }, [dispatch])
     return (
-        <div>
+        <div className='min-h-screen'>
+            {loading && <Loading />}
             {
-                loading ? <Loading />
-                    :
-                    error_ ? <ErrorCard message={error_} />
-                        :
-                        <div className='w-full h-full min-h-screen flex lg:flex-row flex-col justify-between'>
-                            <div className='h-full lg:px-60 md:px-4 px-4 lg:py-20 md:py-10 py-6'>
-                                <div className='pb-6 md:pb-14'>
-                                    <p className="lg:text-4xl text-3xl font-black leading-9 text-gray-800 dark:text-gray-50">Cart</p>
-                                </div>
-                                {
-                                    cartItems.map((cartItem, index) => (
-                                        <CartItemCard key={index} item={cartItem} />
-                                    ))
-                                }
-                            </div>
-                            <CartSummarySection total_price={total_price} total_qty={total_qty} />
+                type !== "error" &&
+                <div className='w-full h-full flex lg:flex-row flex-col justify-between'>
+                    <div className='h-full lg:px-60 md:px-4 px-4 lg:py-20 md:py-10 py-6'>
+                        <div className='pb-6 md:pb-14'>
+                            <p className="lg:text-4xl text-3xl font-black leading-9 text-gray-800 dark:text-gray-50">Cart</p>
                         </div>
+                        {
+                            cartItems.map((cartItem, index) => (
+                                <CartItemCard key={index} item={cartItem} />
+                            ))
+                        }
+                    </div>
+                    <CartSummarySection
+                        total_price={cartItems.reduce((acc, item) => acc + parseFloat(item.price), 0).toFixed(2)}
+                        total_qty={cartItems.reduce((acc, item) => acc + item.qty, 0)}
+                        no_of_items={cartItems.length}
+                    />
+                </div>
             }
+            {message && type === "success" && <SuccessCard message={message} />}
+            {message && type === "error" && <ErrorCard message={message} />}
         </div>
     )
 }

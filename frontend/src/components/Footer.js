287a5ae925055155c6bf309846dc1391e30a4607
ilyasbabu <ilyasbabuvp@gmail.com>
2023-01-05 16:49:52 +0530

    Footer text updated

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -4,8 +4,8 @@ function Footer() {
     return (
         <footer className=" bg-slate-100 dark:bg-black">
             <div className="px-4 py-8 mx-auto max-w-screen-xl sm:px-6 lg:px-8">
-                <p className="mt-4 text-sm text-center text-gray-500 lg:mt-0 dark:text-white">
-                    Copyright &copy; 2022. All rights reserved.
+                <p className="mt-4 text-sm text-center text-gray-500 lg:mt-0 dark:text-slate-100">
+                    Copyright &copy; 2023. All rights reserved by <a href='https://ilyasbabu.github.io/' className='underline'>Ilyas Babu</a>.
                 </p>
             </div>
         </footer>

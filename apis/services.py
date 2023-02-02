2a65015cce6f151f8edf4618b1c13fe4701a1fe4
ilyasbabu <ilyasbabuvp@gmail.com>
2023-02-02 15:43:55 +0530

    abstraction in views.py

--- apis/services.py
+++ apis/services.py
@@ -1,9 +1,20 @@
 import sys
+import json
+import time
 import traceback
 from .models import *
+from rest_framework.response import Response
 from django.core.exceptions import ValidationError
+from django.shortcuts import get_object_or_404
+from .serializers import ProductListSerializer, ProductDetailSerializer, ProductBasicDetailSerializer
 from django.db.models import Count, F, Value
+from django.conf import settings
 
+
+def wait():
+    if settings.DEBUG:
+        print('123')
+        time.sleep(0.5)
 
 def handle_error(e):
     print(e)
@@ -12,19 +23,43 @@ def handle_error(e):
     if isinstance(e, ValidationError):
         error_info = "\n".join(e.messages)
         msg = error_info
-    return msg
+    return json.dumps(str(msg))
 
 
 def get_product_list():
-    products = Product.objects.filter(is_active = True).prefetch_related('product_image').order_by("?")
-    return products
+    try:
+        wait()
+        products = Product.objects.filter(is_active = True).prefetch_related('product_image').order_by("?")
+        serialized_products = ProductListSerializer(products, many=True)
+        return Response(serialized_products.data)
+    except Exception as e:
+        return Response(handle_error(e), status=406)
 
 
 def get_product_detail(slug):
-    product = Product.objects.get(slug=slug, is_active=True)
-    return product
+    try:
+        wait()
+        product = get_object_or_404(Product, slug=slug, is_active=True)
+        serialized_product = ProductDetailSerializer(product)
+        return Response(serialized_product.data)
+    except Exception as e:
+        return Response(handle_error(e), status=406)
+
+
+def get_product_basic_detail(slug):
+    try:
+        wait()
+        product = get_object_or_404(Product, slug=slug, is_active=True)
+        serialized_product = ProductBasicDetailSerializer(product)
+        return Response(serialized_product.data)
+    except Exception as e:
+        return Response(handle_error(e), status=406)
 
 
 def get_stock_count(slug):
-    count = Product.objects.values_list('stock', flat=True).get(slug=slug, is_active=True)
-    return count
+    try:
+        wait()
+        count = Product.objects.values_list('stock', flat=True).get(slug=slug, is_active=True)
+        return Response(json.dumps(count))
+    except Exception as e:
+        return Response(handle_error(e), status=406)
--- apis/views.py
+++ apis/views.py
@@ -1,89 +1,52 @@
-import time
-import json
 from rest_framework.response import Response
 from rest_framework.authentication import SessionAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from django.utils.translation import gettext as _
-from .models import Product
-from django.core.exceptions import ValidationError
-from .serializers import ProductListSerializer, ProductDetailSerializer, ProductBasicDetailSerializer
-from .services import get_product_detail, get_product_list, handle_error, get_stock_count
+from .services import get_product_detail, get_product_list, get_stock_count, get_product_basic_detail
 
-from django.core.exceptions import ValidationError as DjangoValidationError
-from rest_framework.exceptions import ValidationError as DRFValidationError
-from rest_framework.views import exception_handler as drf_exception_handler
 
 # Create your views here.
 
 
 class HomePageAPI(APIView):
     """API for home page"""
-
     authentication_classes = [SessionAuthentication]
     # permission_classes = [IsAuthenticated]
 
     def get(self, request):
-        try:
-            result = get_product_list()
-            serializer = ProductListSerializer(result, many=True)
-            time.sleep(0.5)
-            return Response(serializer.data)
-        except Exception as e:
-            result = handle_error(e)
-            return Response(json.dumps(str(result)),status=400)
+        response = get_product_list()
+        return response
+
 
 
 class ProductDetailPageAPI(APIView):
     """API for product detail page"""
-
     authentication_classes = [SessionAuthentication]
-
     def get(self, request, slug):
-        try:
-            result = get_product_detail(slug)
-            serializer = ProductDetailSerializer(result)
-            time.sleep(0.5)
-            return Response(serializer.data)
-        except Exception as e:
-            result = handle_error(e)
-            return Response(json.dumps(str(result)),status=400)
+        response = get_product_detail(slug)
+        return response
 
 
 class ProductBasicDetailAPI(APIView):
     """API for product basic details"""
-
     authentication_classes = [SessionAuthentication]
 
     def get(self, request, slug):
-        try:
-            result = get_product_detail(slug)
-            serializer = ProductBasicDetailSerializer(result)
-            time.sleep(0.5)
-            return Response(serializer.data)
-        except Exception as e:
-            result = handle_error(e)
-            return Response(json.dumps(str(result)),status=400)
+        response = get_product_basic_detail(slug)
+        return response
 
 
 class CartPageAPI(APIView):
     """API for cart page"""
 
     def get(self, request):
-        try:
-            return Response("Test")
-        except Exception as e:
-            result = handle_error(e)
-            return Response(json.dumps(str(result)),status=400)
+        return Response("Test")
 
 
 class GetStockCountAPI(APIView):
     """API for get stock count of a particular product"""
 
     def get(self, request, slug):
-        try:
-            result = get_stock_count(slug)
-            return Response(json.dumps(result))
-        except Exception as e:
-            result = handle_error(e)
-            return Response(json.dumps(str(result)),status=400)
+        result = get_stock_count(slug)
+        return result

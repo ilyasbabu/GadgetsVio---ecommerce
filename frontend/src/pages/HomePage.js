64d64d2290669e3785495757357bd0dfb965a42b
ilyasbabu <ilyasbabuvp@gmail.com>
2023-03-02 10:09:14 +0530

    PromoCard and FaqCard added in homepage

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -6,6 +6,8 @@ import Loading from '../components/Loading'
 import { useDispatch, useSelector } from 'react-redux'
 import { getProductListAsync } from '../features/productListSlice'
 import { setInitialState } from '../features/commonSlice'
+import PromoBanner from '../components/PromoBanner'
+import FaqCard from '../components/FaqCard'
 
 function HomePage() {
     const { data, loading } = useSelector(state => state.products_list)
@@ -15,7 +17,7 @@ function HomePage() {
     useEffect(() => {
         window.scrollTo(0, 0)
         dispatch(getProductListAsync())
-        return ()=>{
+        return () => {
             dispatch(setInitialState())
         }
     }, [dispatch])
@@ -23,14 +25,19 @@ function HomePage() {
     return (
         <div className='min-h-screen'>
             {loading && <Loading />}
-            {
-                type !== "error" &&
-                <div className="grid justify-center items-center grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-x-4 gap-y-8 place-items-center ">
-                    {products.map(product => (
-                        <ProductCard key={product.slug} product={product} />
-                    ))}
-                </div>
-            }
+            <div>
+                <PromoBanner/>
+                {
+                    type !== "error" &&
+                    <div className="grid justify-center items-center grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-x-4 gap-y-8 place-items-center ">
+                        {products.map(product => (
+                            <ProductCard key={product.slug} product={product} />
+                        ))}
+                    </div>
+                }
+                <FaqCard/>
+            </div>
+
             {message && type === "success" && <SuccessCard message={message} />}
             <div className='flex-col-reverse fixed bottom-4 right-4 w-1/2 sm:w-1/4 '>
                 {
